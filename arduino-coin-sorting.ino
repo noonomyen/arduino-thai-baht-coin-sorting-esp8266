b47dd505359e70160e34b0c0a5cc3fdd785e5163
noonomyen <71913059+noonomyen@users.noreply.github.com>
2022-12-12 13:25:04 +0700

    rename variable

--- arduino-coin-sorting.ino
+++ arduino-coin-sorting.ino
@@ -11,18 +11,19 @@ bool sensor_1_baht = 0;
 bool sensor_5_baht = 0;
 bool sensor_10_baht = 0;
 
-bool pin_reset = 0;
-time_t pin_reset_t;
+bool button = 0;
+time_t button_t;
 
 uint16_t coin_1 = 0;
 uint16_t coin_5 = 0;
 uint16_t coin_10 = 0;
 
-bool beep = 0;
-bool get_beep = 0;
-time_t beep_t;
+bool led = 0;
+bool set_led = 0;
+time_t led_t;
 
 void setup() {
+    Serial.begin(115200);
     LCD::init();
     MEM::begin();
     coin_1 = MEM::read_1_baht();
@@ -45,7 +46,7 @@ void loop() {
         if (sensor_1_baht == 0) {
             sensor_1_baht = 1;
             coin_1 += 1;
-            get_beep = 1;
+            set_led = 1;
             MEM::write_1_baht(coin_1);
             request_lcd_refresh = 1;
         };
@@ -56,7 +57,7 @@ void loop() {
         if (sensor_5_baht == 0) {
             sensor_5_baht = 1;
             coin_5 += 1;
-            get_beep = 1;
+            set_led = 1;
             MEM::write_5_baht(coin_5);
             request_lcd_refresh = 1;
         };
@@ -67,7 +68,7 @@ void loop() {
         if (sensor_10_baht == 0) {
             sensor_10_baht = 1;
             coin_10 += 1;
-            get_beep = 1;
+            set_led = 1;
             MEM::write_10_baht(coin_10);
             request_lcd_refresh = 1;
         };
@@ -75,13 +76,13 @@ void loop() {
         sensor_10_baht = 0;
     };
     if (analogRead(A0) > 512) {
-        get_beep = 1;
-        if (pin_reset == 0) {
-            pin_reset = 1;
-            pin_reset_t = millis();
+        set_led = 1;
+        if (button == 0) {
+            button = 1;
+            button_t = millis();
         } else {
-            if (millis() - pin_reset_t > 5000) {
-                pin_reset_t += 60000;
+            if (millis() - button_t > 5000) {
+                button_t += 60000;
                 MEM::reset();
                 coin_1 = 0;
                 coin_5 = 0;
@@ -90,7 +91,7 @@ void loop() {
             };
         };
     } else {
-        pin_reset = 0;
+        button = 0;
     };
     if (request_lcd_refresh) {
         LCD::set_1_baht(coin_1);
@@ -99,16 +100,16 @@ void loop() {
         LCD::refresh();
         request_lcd_refresh = 0;
     };
-    if (get_beep) {
-        get_beep = 0;
-        beep = 1;
-        beep_t = millis();
+    if (set_led) {
+        set_led = 0;
+        led = 1;
+        led_t = millis();
         digitalWrite(D4, LOW);
     };
-    if (beep) {
-        if (millis() - beep_t > 50) {
+    if (led) {
+        if (millis() - led_t > 50) {
             digitalWrite(D4, HIGH);
-            beep = 0;
+            led = 0;
         };
     };
 };
